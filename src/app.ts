4c201b8e12d65990b71800916a25fc3a5cc57a1f
srichaitanya <srichaitanya.de@gmail.com>
2026-06-11 15:10:41 +0200

    Register product routes

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import productRoutes from "./routes/product.routes";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/products", productRoutes);
 
 app.get("/", (req, res) => {
   res.send("E-Commerce API Running...");
