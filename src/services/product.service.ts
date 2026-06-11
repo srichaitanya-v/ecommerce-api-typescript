c8d9c99a0412e6c402cfcbc6dcb2e1012729e4db
srichaitanya <srichaitanya.de@gmail.com>
2026-06-11 15:08:02 +0200

    Connect service to repository

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,14 +1,5 @@
+import { fetchProducts } from "../repositories/product.repository";
+
 export const getAllProducts = () => {
-  return [
-    {
-      id: 1,
-      name: "iPhone 15",
-      price: 999
-    },
-    {
-      id: 2,
-      name: "Samsung S24",
-      price: 899
-    }
-  ];
+  return fetchProducts();
 };
